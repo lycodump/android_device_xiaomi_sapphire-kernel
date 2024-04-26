d3985fa87265f8507825660e0157db180aacabe1
Ali Hasan <ahb7671@gmail.com>
2024-04-26 17:24:58 +0500

    Refactor extraction script to copy files to modules subfolders and syntax correctness
    - Updated the shell script to organize file copying into subfolders
    within the 'modules' directory.
    - Improved script clarity and structure for better readability and
    maintainability.
    
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -9,7 +9,7 @@ set -e
 
 ### Setup
 DUMP=
-MY_DIR="${BASH_SOURCE%/*}"
+MY_DIR="$(dirname "${BASH_SOURCE[0]}")"
 SRC_ROOT="${MY_DIR}/../../.."
 TMP_DIR=$(mktemp -d)
 EXTRACT_KERNEL=true
@@ -18,17 +18,14 @@ declare -a MODULE_FOLDERS=("vendor_ramdisk" "vendor_dlkm" "system_dlkm")
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
         -n | --no-kernel )
-                EXTRACT_KERNEL=false
-                ;;
+            EXTRACT_KERNEL=false
+            ;;
         * )
-                DUMP="${1}"
-                ;;
+            DUMP="${1}"
+            ;;
     esac
     shift
 done
-
-[ -f "${MY_DIR}/Module.symvers" ] || touch "${MY_DIR}/Module.symvers"
-[ -f "${MY_DIR}/System.map" ] || touch "${MY_DIR}/System.map"
 
 # Check if dump is specified and exists
 if [ -z "${DUMP}" ]; then
@@ -44,10 +41,10 @@ echo "Extracting files from ${DUMP}:"
 ### Kernel
 if ${EXTRACT_KERNEL}; then
     echo "Extracting boot image.."
-    ${SRC_ROOT}/system/tools/mkbootimg/unpack_bootimg.py \
+    "${SRC_ROOT}/system/tools/mkbootimg/unpack_bootimg.py" \
         --boot_img "${DUMP}/boot.img" \
         --out "${TMP_DIR}/boot.out" > /dev/null
-    cp -f "${TMP_DIR}/boot.out/kernel" ${MY_DIR}/kernel
+    cp -f "${TMP_DIR}/boot.out/kernel" "${MY_DIR}/kernel"
     echo "  - kernel"
 fi
 
@@ -57,42 +54,41 @@ rm -rf "${MY_DIR}/dtbs"
 mkdir "${MY_DIR}/dtbs"
 
 echo "Extracting vendor_boot image..."
-${SRC_ROOT}/system/tools/mkbootimg/unpack_bootimg.py \
+"${SRC_ROOT}/system/tools/mkbootimg/unpack_bootimg.py" \
     --boot_img "${DUMP}/vendor_boot.img" \
     --out "${TMP_DIR}/vendor_boot.out" > /dev/null
 
-curl -sSL "https://raw.githubusercontent.com/PabloCastellano/extract-dtb/master/extract_dtb/extract_dtb.py" > ${TMP_DIR}/extract_dtb.py
+curl -sSL "https://raw.githubusercontent.com/PabloCastellano/extract-dtb/master/extract_dtb/extract_dtb.py" > "${TMP_DIR}/extract_dtb.py"
 
 # Copy
 python3 "${TMP_DIR}/extract_dtb.py" "${TMP_DIR}/vendor_boot.out/dtb" -o "${TMP_DIR}/dtbs" > /dev/null
 find "${TMP_DIR}/dtbs" -type f -name "*.dtb" \
     -exec cp {} "${MY_DIR}/dtbs" \; \
-    -exec printf "  - dtbs/" \; \
-    -exec basename {} \;
+    -exec printf "  - dtbs/{}\n" \;
+
 cp -f "${DUMP}/dtbo.img" "${MY_DIR}/dtbs/dtbo.img"
 echo "  - dtbs/dtbo.img"
 
 ### Modules
 # Cleanup / Preparation
-for MODULE_FOLDER in "${MODULE_FOLDERS[@]}"; do
-    rm -rf "${MY_DIR}/${MODULE_FOLDER}"
-    mkdir "${MY_DIR}/${MODULE_FOLDER}"
-done
+rm -rf "${MY_DIR}/modules"
+mkdir "${MY_DIR}/modules"
 
-# Copy
+# Copy files to modules/${MODULE_FOLDER}/
 for MODULE_FOLDER in "${MODULE_FOLDERS[@]}"; do
+    mkdir -p "${MY_DIR}/modules/${MODULE_FOLDER}"
     MODULE_SRC="${DUMP}/${MODULE_FOLDER}"
     if [ "${MODULE_FOLDER}" == "vendor_ramdisk" ]; then
         lz4 -qd "${TMP_DIR}/vendor_boot.out/vendor_ramdisk00" "${TMP_DIR}/vendor_ramdisk.cpio"
         7z x "${TMP_DIR}/vendor_ramdisk.cpio" -o"${TMP_DIR}/vendor_ramdisk" > /dev/null
         MODULE_SRC="${TMP_DIR}/vendor_ramdisk"
     fi
-    [ -d "${MODULE_SRC}" ] || break
-    find "${MODULE_SRC}/lib/modules" -type f \
-        -exec cp {} "${MY_DIR}/${MODULE_FOLDER}/" \; \
-        -exec printf "  - ${MODULE_FOLDER}/" \; \
-        -exec basename {} \;
+    if [ -d "${MODULE_SRC}" ]; then
+        find "${MODULE_SRC}/lib/modules" -type f -exec cp {} "${MY_DIR}/modules/${MODULE_FOLDER}/" \; \
+        -exec printf "  - modules/${MODULE_FOLDER}/{}\n" \;
+    fi
 done
 
 # Clear temp dir
 rm -rf "${TMP_DIR}"
+
